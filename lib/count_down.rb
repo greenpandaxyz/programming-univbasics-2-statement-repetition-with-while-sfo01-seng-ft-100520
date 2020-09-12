db91cf93cac38b4a183f24b385de74118206e11c
Amanda D <amanda@depaula.xyz>
2020-09-12 20:49:25 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,1 +1,13 @@
-# Write your code here
+# magic_exit_number = 7
+# count = 0
+# while count < 10 do
+#   break if count == magic_exit_number
+#   puts "I am the #{count}, I love to count!"
+#   count = count +1
+# end
+
+count = 10
+while count > 0 do
+  puts "Happy New Year!"
+  count -= 1
+end
